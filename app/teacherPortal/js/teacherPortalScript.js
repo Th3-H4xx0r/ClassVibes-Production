74cb224dcf12be782aca4fe43ef38b505e72a17b
Pranav Krishna <krishnatechpranav@gmail.com>
2020-07-20 23:32:52 -0700

    asdf

--- app/teacherPortal/js/teacherPortalScript.js
+++ app/teacherPortal/js/teacherPortalScript.js
@@ -594,16 +594,139 @@ function getWeekStudentAverageReactions_ALL_CLASSES(code){
       })
     }).then(() => {
 
-      console.log("MON: " + monTotal)
+      //Monday
+      let sumMon = monTotal.length != 0 ? monTotal.reduce((previous, current) => current += previous): 0;
+      monAverage = sumMon / monTotal.length;
+      monAverage = sumMon / monTotal.length ? sumMon / monTotal.length: 0
+      
+      //Tuesday
 
-      let sum = monTotal.reduce((previous, current) => current += previous);
-      let avg = sum / monTotal.length;
+      let sumTue = tueTotal.length != 0 ? tueTotal.reduce((previous, current) => current += previous) : 0;
+      tueAverage = sumTue / tueTotal.length;
+      tueAverage = tueAverage? sumTue / tueTotal.length: 0
 
-      console.log(avg)
+      //Wednesday
+      let sumWed = wedTotal.length != 0 ? monTotal.reduce((previous, current) => current += previous): 0;
+      wedAverage = sumWed / wedTotal.length;
+      wedAverage = wedAverage ? wedAverage: 0
 
+      //Thursday
+      let sumThu = thuTotal.length != 0 ? thuTotal.reduce((previous, current) => current += previous): 0;
+      thuAverage = sumThu / thuTotal.length;
+      thuAverage = thuAverage ? thuAverage: 0
 
-    
+      //Friday
+      let sumFri = friTotal.length != 0 ?  friTotal.reduce((previous, current) => current += previous): 0;
+      friAverage = sumFri / friTotal.length;
+      friAverage = friAverage ? friAverage: 0
+
+      //Saturday
+      let sumSat = satTotal.length != 0 ?  satTotal.reduce((previous, current) => current += previous): 0;
+      satAverage = sumSat / satTotal.length;
+      satAverage = satAverage ? satAverage: 0
+
+      //Sunday
+      let sumSun = sunTotal.length != 0 ? sunTotal.reduce((previous, current) => current += previous): 0;
+      sunAverage = sumSun / sunTotal.length;
+      sunAverage = sunAverage ? sunAverage: 0
+
+      console.log(monAverage, tueAverage, wedAverage, thuAverage, friAverage, satAverage, sunAverage)
       console.log('//')
+
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    datasets: [{
+      label: "Average Reaction Index: ",
+      lineTension: 0.3,
+      backgroundColor: "rgba(78, 115, 223, 0.05)",
+      borderColor: "rgba(78, 115, 223, 1)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointBorderColor: "rgba(78, 115, 223, 1)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: [monAverage, tueAverage, wedAverage, thuAverage, friAverage, satAverage, sunAverage],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return value;
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + tooltipItem.yLabel;
+        }
+      }
+    }
+  }
+});
+
     })
 
     
