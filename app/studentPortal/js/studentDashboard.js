e12cb5597b9e37a3476010bc7efcdaf6582a87ef
Pranav Krishna <krishnatechpranav@gmail.com>
2020-07-23 11:49:18 -0700

    asdf

--- app/studentPortal/js/studentDashboard.js
+++ app/studentPortal/js/studentDashboard.js
@@ -784,7 +784,7 @@ function getMeetings(email, pageType) {
   
   
         output = `
-          <div class="col-xl-6 col-md-6 mb-4">
+          <div class="col-xl-12 col-md-6 mb-4">
           <div class="card border-left-primary shadow h-100 py-2">
             <div class="card-body">
               <div class="row no-gutters align-items-center">
