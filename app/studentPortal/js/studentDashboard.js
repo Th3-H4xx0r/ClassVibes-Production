bc760498e470baf4c8501b7bb799b99a81f84cbe
Pranav Krishna <krishnatechpranav@gmail.com>
2020-07-23 12:10:14 -0700

    asdf

--- app/studentPortal/js/studentDashboard.js
+++ app/studentPortal/js/studentDashboard.js
@@ -1101,7 +1101,7 @@ async function getAnnouncements(email, pageType = "annoncements-page-main") {
                 var nameClass = classnamesList[i];
   
                 outputAnnouncements = `
-                <div class="col-xl-6 col-md-6 mb-4">
+                <div class="col-xl-12 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
