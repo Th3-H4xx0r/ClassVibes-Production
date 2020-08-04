a4597a4657cbf05ea98476cc469c3a2c48ec943d
Pranav Krishna <krishnatechpranav@gmail.com>
2020-08-03 23:07:08 -0700

    asdf

--- app/teacherPortal/js/teacherPortalScript.js
+++ app/teacherPortal/js/teacherPortalScript.js
@@ -2318,7 +2318,7 @@ function getChartData(code) {
 
             var unreadMessagesHTML = ''
 
-            if(unreadMessages != undefined && unreadMessages != null && unreadMessages != 0 && unreadMessages != NaN && unreadMessages != 'NaN'){
+            if(unreadMessages, unreadMessages != 0){
               var unreadMessagesHTML =  `<h2><span class="badge badge-primary" style = 'position: absolute; margin-left: 83%; top: 10px'>${unreadMessages}</span><h2></h2>`
 
             }
