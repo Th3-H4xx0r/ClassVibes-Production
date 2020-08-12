f84fee36862dd01391e76ce4f3ee38f20691ec36
Pranav Krishna <krishnatechpranav@gmail.com>
2020-08-11 22:19:58 -0700

    asdf

--- app/studentPortal/js/studentDashboard.js
+++ app/studentPortal/js/studentDashboard.js
@@ -1880,11 +1880,7 @@ function getMessagesForChat_Classes_pageNation(classCode, studentEmail, lastElem
             //console.log(data)
       
             var newMessageUI = `
-        
-
-          
             <div>
-
             <div class="message-component" style=" float: right; margin-left: 50%"  >
               <div class="row"><div style="width: 100%" ></div><p style="color: white; background-color: royalblue; border-radius: 20px 20px 0px 20px; margin-right: 30px; padding: 20px; "><strong>${user}</strong> <br> ${message}</p></div>
             </div>
@@ -1892,9 +1888,7 @@ function getMessagesForChat_Classes_pageNation(classCode, studentEmail, lastElem
             `
     
       var otherMessage = `
-    
       <div>
-
         <div class="message-component" style= " float: left; min-width: 900px"  >
           <div class="row"><div class="container" style="width: 100%"></div><p style="color: black; background-color: #d8e6eb; border-radius: 20px 20px 20px 0px; margin-right: 30px; padding: 20px; "><strong>${user}</strong> <br>${message}</p></div>
         </div>
