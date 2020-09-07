d3cce29c2c2ce7be9ccf57bbee151bdd357803eb
Pranav Krishna <krishnatechpranav@gmail.com>
2020-09-07 12:31:58 -0700

    asdf

--- app/authentication/js/auth.js
+++ app/authentication/js/auth.js
@@ -518,34 +518,6 @@ function emailSignUp(type) {
     
                 }).then(() => {
 
-                    var actionCodeSettings = {
-                    // URL you want to redirect back to. The domain (www.example.com) for this
-                    // URL must be whitelisted in the Firebase Console.
-                    url: 'https://classvibes.net/teacher/dashboard',
-                    // This must be true.
-                    handleCodeInApp: true,
-                    iOS: {
-                        bundleId: 'com.example.ios'
-                    },
-                    android: {
-                        packageName: 'com.example.android',
-                        installApp: true,
-                        minimumVersion: '12'
-                    },
-                    dynamicLinkDomain: 'example.page.link'
-                    };
-
-                    firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-                    .then(function() {
-                        // The link was successfully sent. Inform the user.
-                        // Save the email locally so you don't need to ask the user for it again
-                        // if they open the link on the same device.
-                        window.localStorage.setItem('emailForSignIn', email);
-                    })
-                    .catch(function(error) {
-                        // Some error occurred, you can inspect the code: error.code
-                    });
-                        
                     console.log(loginSuccess);
     
                     if (loginSuccess == true) {
