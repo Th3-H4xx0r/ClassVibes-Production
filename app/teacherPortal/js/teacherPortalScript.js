274f0fa90977dd5106d06933029243a22b24bcfc
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2020-11-07 16:38:04 -0800

    123

--- app/teacherPortal/js/teacherPortalScript.js
+++ app/teacherPortal/js/teacherPortalScript.js
@@ -981,7 +981,7 @@ function getGraphData_Classes_page(code){
 
   document.getElementById('chartBody').innerHTML = `
   <a href='/teacher/classes/${code}' style = 'text-decoration: none'>
-            <div class="card" style="width: 25rem; Box-shadow:0 10px 20px rgba(0,0,0,0.10), 0 6px 6px rgba(0,0,0,0.10); margin-right: 50px; margin-left: 5px; margin-bottom: 40px">
+            <div class="card" style="width: 10rem; Box-shadow:0 10px 20px rgba(0,0,0,0.10), 0 6px 6px rgba(0,0,0,0.10); margin-right: 50px; margin-left: 5px; margin-bottom: 40px">
                 <div class="card-body">
                     <h2><span id = 'unreadMessages${code}'></span></h2>
                   <div class="chart-pie pt-4 pb-2" id = "chartPie${code}">
