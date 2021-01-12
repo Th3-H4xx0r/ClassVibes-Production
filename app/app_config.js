180d0e1cae3afc7a273df397d73745e80c3713d1
Pranav Krishna <krishnatechpranav@gmail.com>
2021-01-11 19:07:41 -0800

    adsf

--- app/app_config.js
+++ app/app_config.js
@@ -10,7 +10,7 @@ function setVersionFooter(){
 
     if(document.getElementById('footer-version-code') != null){
         document.getElementById('footer-version-code').innerHTML = `
-        <span>Copyright &copy; Proto Systems. v${VERSION_CODE}</span>
+        <span>© 2021 Proto Systems. All rights reserved. v${VERSION_CODE}</span>
     `;
     }
 
